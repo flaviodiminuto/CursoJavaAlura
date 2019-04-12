664339aa1d330b11e12588c12c07c864f16f7f5b
flaviodiminuto <flavio.diminuto@outlook.com>
2019-04-11 22:01:13 -0300

    Continuacao do basico

---
+++ src/basico/AliquotaIfs.java
@@ -0,0 +1,12 @@
+/*
+O João gostaria de criar um programa sobre Imposto de Renda (IR) e verificou as regras de alíquota. Ele descobriu no site da receita:
+
+De 1900.0 até 2800.0, o IR é de 7.5% e pode deduzir na declaração o valor de R$ 142
+De 2800.01 até 3751.0, o IR é de 15% e pode deduzir R$ 350
+De 3751.01 até 4664.00, o IR é de 22.5% e pode deduzir R$ 636
+Para começar, o João escreveu o seguinte esboço de classe:
+ */
+package basico;
+
+public class AlicotaIF {
+}
